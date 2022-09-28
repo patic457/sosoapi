6c7d2c690b1a455f0a36b65a549f5717ec0d5cfc
patic457 <patic457@gmail.com>
2022-09-28 13:47:28 +0700

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    // }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
-
 })
-export class AppModule {
-
-}
+export class AppModule {}
