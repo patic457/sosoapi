79d7e691045720c90491e47288de2e6d9b2775f7
patic457 <patic457@gmail.com>
2022-09-29 17:08:35 +0700

    1708

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.listen(parseInt(process.env.APP_PORT) || 4000, function () {
+  app.listen(parseInt(process.env.PORT) || 4000, function () {
     console.log("Server started...... ");
   });
 }
