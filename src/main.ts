6cabb87e73bbd4c681052f5896e8a34018e8d2e9
patic457 <patic457@gmail.com>
2022-09-29 17:03:46 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.listen(process.env.PORT || 4000, function () {
+  app.listen(parseInt(process.env.PORT) || 4000, function () {
     console.log("Server started...... ");
   });
 }
