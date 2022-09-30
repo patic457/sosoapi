e14b22cf52cb4e229b7ba1e7a59738fc1ccb3a7c
patic457 <patic457@gmail.com>
2022-09-30 16:33:06 +0700

    ๅุ_จ

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WebhookModule } from './webhook/webhook.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    WebhookModule
+    WebhookModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      validationSchema:{
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        POSTGRES_USER: process.env.POSTGRES_PASSWORD,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        PORT: Joi.number(),
+      }
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
